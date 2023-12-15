0e61864442380af4f6a9000fd87d8d2c3d962885
Elzaido <91469219+Elzaido@users.noreply.github.com>
2023-12-15 21:06:17 +0300

    main

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'Layout/home_layout.dart';
-import 'firebase_messeging.dart';
+//import 'firebase_messeging.dart';
 import 'firebase_options.dart';
 import 'modules/on_boarding.dart';
 import 'network/local/cache_helper.dart';
@@ -21,7 +21,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  initFirebaseMessaging();
+ // initFirebaseMessaging();
   Widget widget;
   if (onBoarding != null) {
     if (uId != null) {
